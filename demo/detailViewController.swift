6982d2aec136940f86d0c3eeb2db955c119c473f
bearstaro <star@stardeMacBook-Air.local>
2014-08-24 22:48:27 +0800

    ui

---
+++ demo/detailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  detailViewController.swift
+//  demo
+//
+//  Created by star on 14-8-23.
+//  Copyright (c) 2014年 star. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
